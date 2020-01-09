37057e40b35f81572ac0fbef7146eb735019b068
Zidan <midoz393@gmail.com>
2020-01-09 15:23:04 -0500

    added polaris, styled blog template

--- src/templates/Blog/index.js
+++ src/templates/Blog/index.js
@@ -1,15 +1,33 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from 'gatsby-image'
+import styled from '@emotion/styled'
+
+import { Heading } from '@shopify/polaris'
+
+const Text = styled.p`
+    font-family: helvetica;
+    font-size: 1em;
+    padding: 20px;
+`
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
+const imgStyle = {
+    width: '300px',
+}
 
 const Blog = ({ data }) => {
     const blog = data.shopifyArticle
     return(
         <div>
-            <h1>{blog.title}</h1>
-            <p>{blog.content}</p>
-            <p>{blog.id}</p>
-            <Img fluid={blog.image.localFile.childImageSharp.fluid} />
+            <Heading>{blog.title}</Heading>
+            <Wrapper>
+                <Img style={imgStyle} fluid={blog.image.localFile.childImageSharp.fluid} />
+            </Wrapper>
+            <Text>{blog.content}</Text>
         </div>
     )
 }
